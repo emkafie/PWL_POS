c81dcdea8e4346b15a9ccca017f4d3a719f9e075
emkafie <khafi.farel77@gmail.com>
2025-03-03 13:27:43 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,20 +8,36 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+
+        return view('user', ['data' => $user]);
+        
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+        
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        $data = [
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
         // $data = [
         //     'nama' => 'Pelanggan Pertama'
         // ];
