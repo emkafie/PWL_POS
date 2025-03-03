12d62c5d62eef05cb3d64d173eff2cdce657bef0
emkafie <khafi.farel77@gmail.com>
2025-03-03 12:36:06 +0700

    Properti fillable dan guarded

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,20 +12,20 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4,
-        // ];
-        // UserModel::insert($data);
-
         $data = [
-            'nama' => 'Pelanggan Pertama'
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,4 +11,6 @@ class UserModel extends Model
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
