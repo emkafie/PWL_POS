ef90b868777e1607afebaf5b5d7505c3b5186dee
emkafie <khafi.farel77@gmail.com>
2025-03-08 09:30:35 +0700

    Retrieving Aggregates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
         
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::findOr(20, ['username', 'nama'], function () {
