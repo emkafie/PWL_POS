46be34d799ff06dd16dc81c2bb92228d57ad7a53
emkafie <khafi.farel77@gmail.com>
2025-03-08 10:11:52 +0700

    Retrieving or Creating Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,21 +11,39 @@ class UserController extends Controller
     public function index()
     {
 
-        $user = UserModel::where('level_id', 2)->count();
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
         return view('user', ['data' => $user]);
+
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user = UserModel::where('level_id', 2)->count();
         // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        
+
         // $user = UserModel::findOrFail(1);
         // $user = UserModel::findOr(20, ['username', 'nama'], function () {
         //     abort(404);
         // });
-        
+
         // $user = UserModel::firstWhere('level_id', 1);
         // return view('user', ['data' => $user]);
 
         // $user = UserModel::where('level_id', 1)->first();
         // return view('user', ['data' => $user]);
-        
+
         // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
 
