cffe5ebd55f06d53f34b209eb6704e1e88cfea3b
emkafie <khafi.farel77@gmail.com>
2025-03-15 09:48:03 +0700

    Layouting AdminLTE

---
+++ resources/views/layouts/breadcrumb.blade.php
@@ -0,0 +1,14 @@
+<div class="app-content-top-area">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6"><div>App Content Top Area</div></div>
+              <div class="col-md-6 text-end">
+                <button type="submit" class="btn btn-primary" name="save" value="create">
+                  Create Admin
+                </button>
+              </div>
+            </div>
+          </div>
+          <!--end::Container-->
+        </div>
---
+++ resources/views/layouts/mainContent.blade.php
@@ -0,0 +1,56 @@
+<div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-12">
+                <!-- Default box -->
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Title</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                      >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                      </button>
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                      >
+                        <i class="bi bi-x-lg"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">Start creating your amazing application!</div>
+                  <!-- /.card-body -->
+                  <div class="card-footer">Footer</div>
+                  <!-- /.card-footer-->
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+        <!--end::App Content-->
+        <!--begin::App Content Bottom Area-->
+        <div class="app-content-bottom-area">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6"><div>App Content Bottom Area</div></div>
+              <div class="col-md-6 text-end">
+                <button type="submit" class="btn btn-secondary" name="save" value="create">
+                  Refresh
+                </button>
+              </div>
+            </div>
+          </div>
+          <!--end::Container-->
+        </div>
